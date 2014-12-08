8f1d6296f9be743636d39331a2e92720b5c702ab
Matthias Bloch <matthias.bloch@puffin.ch>
2014-12-08 17:23:56 +0100

    first test, skeleton

--- lib/Data/Structure/Validation.pm
+++ lib/Data/Structure/Validation.pm
@@ -1,5 +1,37 @@
 use strict;
 use warnings;
 package Data::Structure::Validation;
+use Carp;
 
+sub new{
+    my $class = shift;
+    my $config = shift || croak '$config not supplied';
+    my $schema = shift || croak '$schema not supplied';
+
+    my $self = {
+        config => $config,
+        schema => $schema,
+    };
+    bless ($self, $class);
+    return $self;
+}
+
+sub parse_config{
+    # load config
+        # determine file or string
+        # parse json
+
+    # load schema
+        # determine file or string
+        # parse json
+
+    # verify config against schema
+
+}
+
+
+
+=pod
+=head1 Validate a Perl Data Structure with a Schema
+=cut
 1;
---
+++ t/001_prereq.t
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+use Test::More tests => 1;
+
+use lib 'lib';
+require_ok ('Data::Structure::Validation');
+
+1;
