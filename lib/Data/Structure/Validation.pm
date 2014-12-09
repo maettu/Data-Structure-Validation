fb26474ca80d43072013c4e3f030d508508ac76a
Matthias Bloch <matthias.bloch@puffin.ch>
2014-12-09 14:58:43 +0100

    check_mandatory debug version, incomplete

--- lib/Data/Structure/Validation.pm
+++ lib/Data/Structure/Validation.pm
@@ -1,3 +1,4 @@
+use 5.10.1;
 use strict;
 use warnings;
 package Data::Structure::Validation;
@@ -16,17 +17,75 @@ sub new{
     return $self;
 }
 
-sub parse_config{
-    # load config
-        # determine file or string
-        # parse json
+sub validate{
+    my $self   = shift;
+    my $config = $self->{config};
+    my $schema = $self->{schema};
 
-    # load schema
-        # determine file or string
-        # parse json
+    # check if everything in config is in line with schema
 
-    # verify config against schema
+    for my $key (keys %{$config}){
+        say $key;
 
+        # Unterschied zwischen expliziten keys (GENERAL) und solchen, die
+        # "nur" da sein müssen ("silo-a", "silo-b" usw)
+        if (exists $schema->{$key}){
+            say "$key gefunden";
+#~             use Data::Dumper; say Dumper $schema->{$key};
+            for my $schema_key (keys %{$schema->{$key}}){
+                say "    $schema_key";
+            }
+        }
+        else {
+            say "$key not there";
+        }
+
+    }
+
+    # check if all mandatory fields are there
+    say "###############";
+    _check_mandatory($config, $schema, 0);
+    say "###############";
+
+#~     use Data::Dumper; say Dumper $schema;
+}
+
+sub _check_mandatory {
+    my $config = shift;
+    my $schema = shift;
+    my $depth  = shift // 0;
+
+
+    for my $key (keys %{$schema}){
+
+        # check here
+
+        print ' ' x $depth, $key;
+
+        if (ref $schema->{$key} eq ref {}){
+            if (exists $schema->{$key}->{mandatory}
+                    and
+                $schema->{$key}->{mandatory}){
+
+                print "  ($key is mandatory)";
+
+                say "\n*** Error *** $key not found"
+                    unless (exists $config->{$key});
+
+            }
+            print "\n";
+
+            if ($key eq 'members'){
+                say "wohoo 'members'";
+                # injecting keys for all members found in $config into $schema?
+            }
+
+            _check_mandatory($config->{$key}, $schema->{$key}, $depth+4)
+        }
+        else {
+            say ": $schema->{$key}";
+        }
+    }
 }
 
 
---
+++ t/002_validate.t
@@ -0,0 +1,75 @@
+#!/usr/bin/perl
+use Test::More;
+
+use lib 'lib';
+use Data::Structure::Validation;
+
+my $config = {
+    GENERAL => {
+        logfile => '/tmp/n3k-poller.log',
+        cachedb => '/tmp/n3k-cache.db',
+        history => '3d',
+        silos   => {
+            'silo-a' => {
+                url => 'https://silo-a/api',
+                key => 'my-secret-shared-key',
+            }
+        }
+
+    }
+};
+
+my $schema = {
+    GENERAL => {
+        mandatory   => 1,
+        description =>  'general settings',
+        logfile => {
+            value       =>  'qx{^/}',
+            description =>  'absolute path to logfile',
+            mandatory   => 1,
+        },
+        cachedb => {
+            value => 'qx{^/}',
+            description => 'absolute path to cache (sqlite) database file',
+            mandatory => 1,
+        },
+        history => {
+            mandatory => 1,
+        },
+        silos => {
+            mandatory   => 1,
+            description => 'silos store collected data',
+            # "members" stands for all "non-internal" fields
+            members => {
+                url => {
+                    mandatory   => 1,
+                    value       => 'qx{^https}',
+                    description => 'url of the silo server. Only https:// allowed',
+                },
+                key => {
+                    mandatory   => 1,
+                    description => 'shared secret to identify node'
+                }
+            }
+
+        }
+    }
+};
+
+eval {Data::Structure::Validation->new(undef)};
+ok ($@, '$config not supplied');
+
+eval {Data::Structure::Validation->($config, undef)};
+ok ($@, '$schema not supplied');
+
+my $validator = Data::Structure::Validation->new($config, $schema);
+isa_ok( $validator, Data::Structure::Validation, '$checker' );
+
+can_ok($validator, validate);
+
+$validator->validate();
+
+
+
+done_testing();
+
